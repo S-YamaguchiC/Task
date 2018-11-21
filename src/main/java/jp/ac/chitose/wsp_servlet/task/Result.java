90292aecefff341453b7f0e7b633aef3626197e3
S-YamaguchiC <cist.b216.s.yamaguchi@gmail.com>
2018-11-21 19:16:37 +0900

    結果表示画面（予定）

---
+++ src/main/java/jp/ac/chitose/wsp_servlet/task/Result.java
@@ -0,0 +1,10 @@
+package jp.ac.chitose.wsp_servlet.task;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/result")
+public class Result extends HttpServlet {
+
+    //いつもの
+}
