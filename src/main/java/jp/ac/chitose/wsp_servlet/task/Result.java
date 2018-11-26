18f94ea837092449bf15e543c1f5d6d51e9f8352
S-YamaguchiC <cist.b216.s.yamaguchi@gmail.com>
2018-11-26 12:13:34 +0900

    結果画面

--- src/main/java/jp/ac/chitose/wsp_servlet/task/Result.java
+++ src/main/java/jp/ac/chitose/wsp_servlet/task/Result.java
@@ -2,9 +2,30 @@ package jp.ac.chitose.wsp_servlet.task;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/result")
 public class Result extends HttpServlet {
 
     //いつもの
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+
+        HttpSession session = req.getSession();
+
+        try(PrintWriter out = resp.getWriter() ) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html><head>");
+            out.println("<meta charset=\"UTF-8\">");
+            out.println("</head><body>");
+            out.println("<p>試合終了～" + session.getAttribute("winner") + "</p>");
+            out.println("<a href=\"././register\">トップに戻る</a>");
+            out.println("</body></html>");
+        }
+    }
 }
