e09217e1e4efc0dd951efb299be7f7a6c1b71adf
S-YamaguchiC <cist.b216.s.yamaguchi@gmail.com>
2018-11-26 12:11:38 +0900

    ArrayListに対してのremoveができていない？（要修正）

--- src/main/java/jp/ac/chitose/wsp_servlet/task/CreateRand.java
+++ src/main/java/jp/ac/chitose/wsp_servlet/task/CreateRand.java
@@ -1,6 +1,7 @@
 package jp.ac.chitose.wsp_servlet.task;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -22,6 +23,7 @@ public class CreateRand {
     // CPUの座標
     public String createComCoords() {
         // 座標のランダム出力(1~5), 縦横
+        // nextInt(int val) -> 0 ~ val-1 までの値で乱数
         r_len = random.nextInt(5) + 1;
         r_wid = random.nextInt(5) + 1;
         // ret (HTMLの<td>タグのidに相当)
@@ -31,22 +33,45 @@ public class CreateRand {
     // CPUの攻撃座標
     public String createAttCoords() {
         // 座標をランダム出力(1~5), 縦横
-        r_att_len = random.nextInt(5) + 1;
-        r_att_wid = random.nextInt(5) + 1;
+        // 既に使われた座標の場合はもう一回ランダム
+        while(true) {
+            // 0 ~ 5-1
+            r_att_len = random.nextInt(5);
+            r_att_wid = random.nextInt(5);
+            if ( attResultArray.get(r_att_len + r_att_wid).isEmpty() ) {
+                // なにもしない、ループs
+            } else {
+                break;
+            }
+        }
         // すべての攻撃パターンから、今出力した値を削除(remove)
-        attResultArray.remove(r_att_len +  "," + r_att_wid);    // 1,2 or 0,4 or 3,2 etc...
+        String val = (r_att_len+1) + "," + (r_att_wid+1);
+        try {
+            // 0が入るとcatchになってしまう
+            attResultArray.remove(val);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception(val) -> " + val);
+        }
+        // 1,2 or 0,4 or 3,2 etc...
         // ret (HTMLの<td>タグのidに相当)
-        return String.valueOf(r_att_len + r_att_wid);
+//        System.out.println("Computer ->" + r_att_len + ", " + r_att_wid + "\n");
+        return String.valueOf(r_att_len+1) + String.valueOf(r_att_wid+1);
     }
 
     // CPUの全攻撃パターンをArrayListに格納
     public void allAttPatern() {
-
+        // 0~4 の5通り^2 = 25パターン
+        // 1~5 に変更
+        int index = 0;
         for(int i=0; i<5; i++) {
             for(int j=0; j<5; j++) {
-                attResultArray.add(i+j, String.valueOf(i) + "," + String.valueOf(j));   // 1,2 or 0,4 or 3,2 etc...
+                String val = (i+1) + "," + (j+1);
+//                System.out.println(val);
+                attResultArray.add(index, val);
+                index++;
             }
         }
+        System.out.println(Arrays.toString(attResultArray.toArray()));
     }
 
     // getter
