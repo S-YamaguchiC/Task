054582dab380ce17a80ed8edde993ae422d69072
S-YamaguchiC <cist.b216.s.yamaguchi@gmail.com>
2018-12-10 02:32:42 +0900

    攻撃座標の検索と削除

--- src/main/java/jp/ac/chitose/wsp_servlet/task/dao/HistoryDAO.java
+++ src/main/java/jp/ac/chitose/wsp_servlet/task/dao/HistoryDAO.java
@@ -43,6 +43,46 @@ public class HistoryDAO implements IHistoryDAO {
     }
 
     @Override
+    public List<String> selectPlayerAttackHistory() {
+
+        List<String> pAttackArray = new ArrayList<>();
+        String sql = "select P_HISTORY from PLAYERHISTORY";
+
+        //noinspection Duplicates
+        try( Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWD)) {
+            try(PreparedStatement ppst = connection.prepareStatement(sql)) {
+                ResultSet rs = ppst.executeQuery();
+                while (rs.next()) {
+                    pAttackArray.add(rs.getString(1));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return pAttackArray;
+    }
+
+    @Override
+    public List<String> selectComputerAttackHistory() {
+
+        List<String> cAttackArray = new ArrayList<>();
+        String sql = "select C_HISTORY from COMPUTERHISTORY";
+
+        //noinspection Duplicates
+        try( Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWD)) {
+            try (PreparedStatement ppst = connection.prepareStatement(sql)){
+                ResultSet rs = ppst.executeQuery();
+                while (rs.next()) {
+                    cAttackArray.add(rs.getString(1));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return cAttackArray;
+    }
+
+    @Override
     public void insertGameResult(String result) {
 
         String sql = "insert into RESULTS (RESULT) values (?)";
@@ -75,6 +115,23 @@ public class HistoryDAO implements IHistoryDAO {
 
         }
         return resultArray;
+    }
+
+    @Override
+    public void deleteAttackHistory() {
+
+        String sql1 = "delete from PLAYERHISTORY";
+        String sql2 = "delete from COMPUTERHISTORY";
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWD)){
+            try (PreparedStatement ppst1 = connection.prepareStatement(sql1)){
+                ppst1.executeUpdate();
+            }
+            try (PreparedStatement ppst2 = connection.prepareStatement(sql2)){
+                ppst2.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
--- src/main/java/jp/ac/chitose/wsp_servlet/task/dao/IHistoryDAO.java
+++ src/main/java/jp/ac/chitose/wsp_servlet/task/dao/IHistoryDAO.java
@@ -17,6 +17,18 @@ public interface IHistoryDAO {
     void insertComputerHistory(String computerAttackHistory);
 
     /**
+     * Playerの攻撃座標を出力
+     * @return
+     * */
+    List<String> selectPlayerAttackHistory();
+
+    /**
+     * Computerの攻撃座標を出力
+     * @return
+     * */
+    List<String> selectComputerAttackHistory();
+
+    /**
      * 引数をもとに試合の結果を保存する
      * @param result 試合結果
      * */
@@ -24,6 +36,12 @@ public interface IHistoryDAO {
 
     /**
      * 過去すべての試合結果を取得する
+     * @return
      * */
     List<String> selectGameResults();
+
+    /**
+     * 試合終了時に攻撃座標テーブルのレコードをすべて削除
+     * */
+    void deleteAttackHistory();
 }
